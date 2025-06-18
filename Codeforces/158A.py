a03006482d8141b81e83e586b654e4ea728c582d
Nafiul Islam <nafiulislam490@gmail.com>
2025-06-18 12:41:29 +0600

    Create 158A.py

---
+++ Codeforces/158A.py
@@ -0,0 +1,9 @@
+#https://codeforces.com/problemset/problem/158/A
+
+n, k = map(int, input().split())
+scores = list(map(int, input().split()))
+
+threshold = scores[k - 1]
+count = sum(1 for score in scores if score >= threshold and score > 0)
+
+print(count)
