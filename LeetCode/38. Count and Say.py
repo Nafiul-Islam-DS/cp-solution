591cdb2a32f25b0a2820befe5253e75e5772ddfc
Nafiul Islam <nafiulislam490@gmail.com>
2025-06-25 17:24:31 +0600

    Create 38. Count and Say.py

---
+++ LeetCode/38. Count and Say.py
@@ -0,0 +1,21 @@
+#https://leetcode.com/problems/count-and-say/
+
+
+class Solution(object):
+    def countAndSay(self, n):
+        if n == 1:
+            return "1"
+        
+        prev = self.countAndSay(n - 1)  # Use self. here
+        result = ""
+        count = 1
+
+        for i in range(1, len(prev)):
+            if prev[i] == prev[i - 1]:
+                count += 1
+            else:
+                result += str(count) + prev[i - 1]
+                count = 1
+
+        result += str(count) + prev[-1]
+        return result
